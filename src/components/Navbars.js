75b72994d21e2c46ba597ec7476c7007028525e9
Parimal-Tank <parimalt@zignuts.com>
2023-03-30 14:33:13 +0530

    Bug Fix

--- src/components/Navbars.js
+++ src/components/Navbars.js
@@ -1,7 +1,7 @@
 import { Button } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
-import { Navbar, NavbarBrand } from "reactstrap";
+import { Navbar } from "reactstrap";
 
 const Navbars = () => {
 
@@ -14,9 +14,9 @@ const Navbars = () => {
   return (
     <div className="px-5 py-1 navbar-style">
       <Navbar>
-        <NavbarBrand href="/dashboard" className="text-white">
+        <a href="/dashboard" className="text-white text-decoration-none h4">
           My Online Shopping Site
-        </NavbarBrand>
+        </a>
         <div>
         <Link to='/dashboard/editprofile'  className="text-decoration-none text-white"><Button  className="nav-button" id='edit-user-profile'  variant="contained" outline>Edit Profile</Button></Link>
 
